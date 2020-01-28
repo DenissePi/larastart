f4ef413af62e47fc5f687ba54cae0499c4f3eb10
DenissePi <52550133+DenissePi@users.noreply.github.com>
2020-01-28 09:51:53 -0600

    history and colors

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,6 +7,25 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+let routes = [{
+        path: '/dashboard',
+        component: require('./components/Dashboard.vue').default
+    },
+    {
+        path: '/profile',
+        component: require('./components/Profile.vue').default
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+})
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,5 +47,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
